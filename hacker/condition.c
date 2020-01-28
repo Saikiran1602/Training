cbd082ca33cbdb495da45760cf4ea34d87747329
Saikiran1602 <60145902+Saikiran1602@users.noreply.github.com>
2020-01-28 10:57:02 +0530

    Create condition.c

---
+++ hacker/condition.c
@@ -0,0 +1,16 @@
+int main()
+{
+    int data;
+    scanf("%d",&data);
+    if(data%2==1||data>5&&data<21)
+    {
+        printf("Weird");
+    }
+    else
+    {
+        printf("Not Weird");
+    
+    }
+    
+     return 0;
+}
